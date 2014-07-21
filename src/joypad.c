ef9b5a0a3dc9e584eef78aab14eaf50251e1e840
ck <ck@gps-e.com>
2014-07-21 16:14:14 +0800

    modify files.

--- src/joypad.c
+++ src/joypad.c
@@ -1,5 +1,5 @@
 // 包含头文件
-#include "joypad.h"
+#include "nes.h"
 
 // 内部函数实现
 #define TURBOKEY_THREAD_DELAY  200
@@ -44,6 +44,7 @@ void joypad_free(JOYPAD *jp)
 
 void joypad_reset(JOYPAD *jp)
 {
+    jp->strobe       = FALSE;
     jp->counter_4016 = 0;
     jp->counter_4017 = 0;
     jp->pad_data[0]  = 0;
@@ -59,9 +60,21 @@ void joypad_setkey(JOYPAD *jp, int pad, int key, int value)
     BOOL flag = FALSE;
     int  i;
 
+    //++ handle pad connect/disconnect ++//
+    if (key == NES_PAD_CONNECT)
+    {
+        if (value) jp->pad_data[pad] =  NES_PAD_CONNECT;
+        else       jp->pad_data[pad] = ~NES_PAD_CONNECT;
+        return;
+    }
+    //-- handle pad connect --//
+
+    //++ handle pad key up/down ++//
     if (value) jp->pad_data[pad] |=  key;
     else       jp->pad_data[pad] &= ~key;
+    //-- handle pad key up/down --//
 
+    //++ handle turbo keys ++//
     for (i=0; i<4; i++)
     {
         if (jp->pad_data[i] & (0x3 << 24))
@@ -77,13 +90,72 @@ void joypad_setkey(JOYPAD *jp, int pad, int key, int value)
         else    ResetEvent(jp->hTurboEvent);
         jp->bTurboFlag = flag;
     }
+    //-- handle turbo keys --//
 }
 
 void NES_PAD_REG_RCB(MEM *pm, int addr)
 {
+    NES    *nes = container_of(pm, NES, ppuregs);
+    JOYPAD *pad = &(nes->pad);
+
+    if (pad->strobe) pm->data[addr] = 0;
+    else
+    {
+        BYTE conn = 0;
+        conn = (pad->pad_data[3] & NES_PAD_CONNECT) >> 26; conn <<= 1;
+        conn = (pad->pad_data[2] & NES_PAD_CONNECT) >> 26; conn <<= 1;
+        conn = (pad->pad_data[1] & NES_PAD_CONNECT) >> 26; conn <<= 1;
+        conn = (pad->pad_data[0] & NES_PAD_CONNECT) >> 26; conn <<= 0;
+
+        switch (addr)
+        {
+        case 0: // 4016
+            if (pad->counter_4016 < 8) {
+                pm->data[addr] = (pad->pad_data[0] >> (pad->counter_4016 - 0)) & 0x1;
+            }
+            else if (pad->counter_4016 < 16) {
+                pm->data[addr] = (pad->pad_data[2] >> (pad->counter_4016 - 8)) & 0x1;
+            }
+            else if (pad->counter_4016 < 20) {
+                pm->data[addr] = conn >> (pad->counter_4016 - 16) & 0x1;
+            }
+            else {
+                pm->data[addr] = 0;
+            }
+            if (pad->counter_4016 < 32) pad->counter_4016++;
+            break;
+        case 1: // 4017
+            if (pad->counter_4017 < 8) {
+                pm->data[addr] = (pad->pad_data[1] >> (pad->counter_4017 - 0)) & 0x1;
+            }
+            else if (pad->counter_4017 < 16) {
+                pm->data[addr] = (pad->pad_data[3] >> (pad->counter_4017 - 8)) & 0x1;
+            }
+            else if (pad->counter_4017 < 20) {
+                pm->data[addr] = conn >> (pad->counter_4017 - 16) & 0x1;
+            }
+            else {
+                pm->data[addr] = 0;
+            }
+            if (pad->counter_4017 < 32) pad->counter_4017++;
+            break;
+        }
+    }
 }
 
 void NES_PAD_REG_WCB(MEM *pm, int addr)
 {
+    NES    *nes = container_of(pm, NES, ppuregs);
+    JOYPAD *pad = &(nes->pad);
+
+    switch (addr)
+    {
+    case 0: // 4016
+        if (pm->data[addr] & 0x1) {
+            pad->counter_4016 = 0;
+            pad->counter_4017 = 0;
+        }
+        break;
+    }
 }
 
--- src/joypad.h
+++ src/joypad.h
@@ -6,23 +6,21 @@
 #include "mem.h"
 
 // 常量定义
-#define NES_KEY_A          (1 << 0)
-#define NES_KEY_B          (1 << 1)
-#define NES_KEY_SELECT     (1 << 2)
-#define NES_KEY_START      (1 << 3)
-#define NES_KEY_UP         (1 << 4)
-#define NES_KEY_DOWN       (1 << 5)
-#define NES_KEY_LEFT       (1 << 6)
-#define NES_KEY_RIGHT      (1 << 7)
-#define NES_PAD0_CONNECTED (1 << 16)
-#define NES_PAD1_CONNECTED (1 << 17)
-#define NES_PAD2_CONNECTED (1 << 18)
-#define NES_PAD3_CONNECTED (1 << 19)
-#define NES_KEY_TURBO_A    (1 << 24)
-#define NES_KEY_TURBO_B    (1 << 25)
+#define NES_KEY_A       (1 << 0)
+#define NES_KEY_B       (1 << 1)
+#define NES_KEY_SELECT  (1 << 2)
+#define NES_KEY_START   (1 << 3)
+#define NES_KEY_UP      (1 << 4)
+#define NES_KEY_DOWN    (1 << 5)
+#define NES_KEY_LEFT    (1 << 6)
+#define NES_KEY_RIGHT   (1 << 7)
+#define NES_KEY_TURBO_A (1 << 24)
+#define NES_KEY_TURBO_B (1 << 25)
+#define NES_PAD_CONNECT (1 << 26)
 
 // 类型定义
 typedef struct {
+    BOOL   strobe;
     DWORD  counter_4016;
     DWORD  counter_4017;
     DWORD  pad_data[4];
