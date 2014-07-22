6b114e46087ba9fc58c7f101f2ed61177484321e
ck <ck@gps-e.com>
2014-07-22 14:33:43 +0800

    modify files

--- src/emulator.c
+++ src/emulator.c
@@ -4,23 +4,16 @@
 // 内部常量定义
 #define SCREEN_WIDTH    GetSystemMetrics(SM_CXSCREEN)
 #define SCREEN_HEIGHT   GetSystemMetrics(SM_CYSCREEN)
-#define WIN_WIDTH       256
-#define WIN_HEIGHT      240
+#define NES_WIDTH       256
+#define NES_HEIGHT      240
 #define APP_CLASS_NAME  "ffnes_emulator"
 #define APP_WND_TITLE   "ffnes"
 
 // 内部函数实现
 static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
-    PAINTSTRUCT ps = {0};
-    HDC        hdc = NULL;
-
     switch (msg)
     {
-    case WM_PAINT:
-        hdc = BeginPaint(hwnd, &ps);
-        EndPaint(hwnd, &ps);
-        break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -40,13 +33,15 @@ int WINAPI WinMain(HINSTANCE hCurInst, HINSTANCE hPreInst, LPSTR lpCmdLine, int 
     // 注册窗口
     wc.lpfnWndProc   = WndProc;
     wc.hInstance     = hCurInst;
+    wc.hIcon         = LoadIcon  (NULL, IDI_APPLICATION);
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
     wc.lpszClassName = APP_CLASS_NAME;
     wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
     if (!RegisterClass(&wc)) return FALSE;
 
     // 创建窗口
-    int winwidth  = WIN_WIDTH + 64;
-    int winheight = WIN_WIDTH + 64;
+    int winwidth  = NES_WIDTH  + 64;
+    int winheight = NES_HEIGHT + 64;
     int winxpos   = (SCREEN_WIDTH  - winwidth ) / 2;
     int winypos   = (SCREEN_HEIGHT - winheight) / 2;
     hwnd = CreateWindowEx(
