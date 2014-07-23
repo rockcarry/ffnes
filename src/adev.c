611abc1c7cbfb1c50da36c329fccd5a07f30d419
ck <ck@gps-e.com>
2014-07-23 11:32:03 +0800

    implement adev

--- src/adev.c
+++ src/adev.c
@@ -1,1 +1,104 @@
-// todo..
+// 包含头文件
+#include "adev.h"
+
+// 内部类型定义
+typedef struct
+{
+    HWAVEOUT hWaveOut;
+    WAVEHDR *pWaveHdr;
+    int      bufnum;
+    int      buflen;
+    HANDLE   bufsem;
+    long     head;
+    long     tail;
+} ADEV;
+
+// 内部函数实现
+static void CALLBACK waveOutProc(HWAVEOUT hwo, UINT uMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2)
+{
+    ADEV *dev = (ADEV*)dwInstance;
+    switch (uMsg)
+    {
+    case WOM_DONE:
+        dev->pWaveHdr[dev->head].dwBufferLength = dev->buflen;
+        if (++dev->head == dev->bufnum) dev->head = 0;
+        ReleaseSemaphore(dev->bufsem, 1, NULL);
+        break;
+    }
+}
+
+// 函数实现
+void* adev_create(int bufnum, int buflen)
+{
+    ADEV *dev = malloc(sizeof(ADEV));
+    if (dev)
+    {
+        WAVEFORMATEX wfx = {0};
+        BYTE        *pwavbuf;
+        int          i;
+
+        // create semaphore
+        dev->bufsem = CreateSemaphore(NULL, bufnum, bufnum, NULL);
+
+        // init for audio
+        wfx.cbSize          = sizeof(wfx);
+        wfx.wFormatTag      = WAVE_FORMAT_PCM;
+        wfx.wBitsPerSample  = 16;    // 16bit
+        wfx.nSamplesPerSec  = 44100; // 44.1k
+        wfx.nChannels       = 2;     // stereo
+        wfx.nBlockAlign     = wfx.nChannels * wfx.wBitsPerSample / 8;
+        wfx.nAvgBytesPerSec = wfx.nBlockAlign * wfx.nSamplesPerSec;
+        waveOutOpen(&(dev->hWaveOut), WAVE_MAPPER, &wfx, (DWORD_PTR)waveOutProc, (DWORD)dev, CALLBACK_FUNCTION);
+
+        dev->bufnum   = bufnum;
+        dev->buflen   = buflen;
+        dev->pWaveHdr = (WAVEHDR*)malloc(bufnum * (sizeof(WAVEHDR) + buflen));
+        dev->head     = 0;
+        dev->tail     = 0;
+
+        // init
+        pwavbuf = (BYTE*)(dev->pWaveHdr + bufnum);
+        for (i=0; i<bufnum; i++) {
+            dev->pWaveHdr[i].lpData         = (LPSTR)(pwavbuf + i * buflen);
+            dev->pWaveHdr[i].dwBufferLength = buflen;
+            waveOutPrepareHeader(dev->hWaveOut, &(dev->pWaveHdr[i]), sizeof(WAVEHDR));
+        }
+    }
+    return dev;
+}
+
+void adev_destroy(void *ctxt)
+{
+    int i;
+    ADEV *dev = (ADEV*)ctxt;
+    if (dev == NULL) return;
+
+    // unprepare
+    for (i=0; i<dev->bufnum; i++) {
+        waveOutUnprepareHeader(dev->hWaveOut, &(dev->pWaveHdr[i]), sizeof(WAVEHDR));
+    }
+
+    if (dev->hWaveOut) waveOutClose(dev->hWaveOut);
+    if (dev->pWaveHdr) free(dev->pWaveHdr);
+    if (dev->bufsem  ) CloseHandle(dev->bufsem);
+    free(dev);
+}
+
+void adev_audio_buf_request(void *ctxt, AUDIOBUF **ppab)
+{
+    ADEV *dev = (ADEV*)ctxt;
+    if (dev == NULL) return;
+
+    WaitForSingleObject(dev->bufsem, -1);
+    *ppab = &(dev->pWaveHdr[dev->tail]);
+}
+
+void adev_audio_buf_post(void *ctxt, AUDIOBUF **ppab)
+{
+    ADEV *dev = (ADEV*)ctxt;
+    if (dev == NULL) return;
+
+    waveOutWrite(dev->hWaveOut, (LPWAVEHDR)*ppab, sizeof(WAVEHDR));
+    if (++dev->tail == dev->bufnum) dev->tail = 0;
+}
+
--- src/adev.h
+++ src/adev.h
@@ -1,1 +1,19 @@
-// todo..
+#ifndef _NES_ADEV_H_
+#define _NES_ADEV_H_
+
+// 包含头文件
+#include "stdefine.h"
+
+// 类型定义
+typedef struct {
+    BYTE  *lpdata;
+    DWORD  buflen;
+} AUDIOBUF;
+
+// 函数声明
+void* adev_create (int bufnum, int buflen);
+void  adev_destroy(void *ctxt);
+void  adev_audio_buf_request(void *ctxt, AUDIOBUF **ppab);
+void  adev_audio_buf_post   (void *ctxt, AUDIOBUF **ppab);
+
+#endif
