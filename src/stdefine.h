58cc63581ef1680e4e9236c55d588f41f2d878f8
ck <ck@gps-e.com>
2014-08-22 20:36:36 +0800

    modify stdefine.h for vs2005, disable some compile warnning.

--- src/stdefine.h
+++ src/stdefine.h
@@ -4,6 +4,12 @@
 
 #if defined(WIN32)
 #include <windows.h>
+
+#ifdef _MSC_VER
+#pragma warning(disable:4311)
+#pragma warning(disable:4312)
+#pragma warning(disable:4996)
+#endif
 #else
 
 /* 常量定义 */
@@ -19,8 +25,13 @@ typedef long     int   LONG;
 
 #endif
 
+#ifndef offsetof
 #define offsetof(type, member)          ((size_t)&((type*)0)->member)
+#endif
+
+#ifndef container_of
 #define container_of(ptr, type, member) (type*)((char*)(ptr) - offsetof(type, member))
+#endif
 
 /* 该宏用于消除变量未使用的警告 */
 #define DO_USE_VAR(var)  do { var = var; } while (0)
