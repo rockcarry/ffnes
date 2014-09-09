5f1f28338fd743ca8b4a98ed2519d92adfff2aca
ck <ck@gps-e.com>
2014-09-09 23:07:37 +0800

    fix mem read & write wrap around issue.

--- src/mem.c
+++ src/mem.c
@@ -4,30 +4,36 @@
 // º¯ÊýÊµÏÖ
 BYTE mem_readb(MEM *pm, int addr)
 {
-    // memory read callback
+    // wrap around
+    addr %= pm->size;
+
     if (pm->r_callback) return pm->r_callback(pm, addr);
-    if (pm->data      ) return pm->data[addr % pm->size];
+    if (pm->data      ) return pm->data[addr];
     return 0;
 }
 
 WORD mem_readw(MEM *pm, int addr)
 {
-    // memory read callback
+    // wrap around
+    addr %= pm->size;
+
     if (pm->r_callback) {
         WORD word;
         word  = pm->r_callback(pm, addr + 0) << 0;
         word |= pm->r_callback(pm, addr + 1) << 8;
         return word;
     }
-
-    if (pm->data) return *(WORD*)(pm->data + addr % pm->size);
+    if (pm->data) return *(WORD*)(pm->data + addr);
     return 0;
 }
 
 void mem_writeb(MEM *pm, int addr, BYTE byte)
 {
+    // wrap around
+    addr %= pm->size;
+
     if (pm->type != MEM_ROM) {
-        if (pm->data) pm->data[addr % pm->size] = byte;
+        if (pm->data) pm->data[addr] = byte;
     }
 
     // memory write callback
@@ -36,8 +42,11 @@ void mem_writeb(MEM *pm, int addr, BYTE byte)
 
 void mem_writew(MEM *pm, int addr, WORD word)
 {
+    // wrap around
+    addr %= pm->size;
+
     if (pm->type != MEM_ROM) {
-        if (pm->data) *(WORD*)(pm->data + addr % pm->size) = word;
+        if (pm->data) *(WORD*)(pm->data + addr) = word;
     }
 
     // memory write callback
