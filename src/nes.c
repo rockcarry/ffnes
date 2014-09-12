6c1341dc3b3a34a34df9632c2279caeec638ad79
ck <ck@gps-e.com>
2014-09-12 13:27:30 +0800

    modify file.

--- src/nes.c
+++ src/nes.c
@@ -42,7 +42,7 @@ static DWORD WINAPI nes_thread_proc(LPVOID lpParam)
 
         totalpclk = NES_HTOTAL * NES_VTOTAL;
         do {
-            torunpclk  = (totalpclk < 16) ? totalpclk : 16;
+            torunpclk  = (totalpclk < 32) ? totalpclk : 32;
             totalpclk -= torunpclk;
 
             apu_run_pclk(&(nes->apu), torunpclk);
