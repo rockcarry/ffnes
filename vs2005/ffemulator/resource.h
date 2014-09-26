94c44a3bbc34b4ccc10150e0ad3fee3cb68afe08
ck <ck@gps-e.com>
2014-09-26 18:40:35 +0800

    add more button for ffndb debugging dialog.

--- vs2005/ffemulator/resource.h
+++ vs2005/ffemulator/resource.h
@@ -10,23 +10,28 @@
 #define IDC_BTN_NES_RUN_PAUSE           1001
 #define IDC_BTN_NES_DEBUG_CPU           1002
 #define IDC_BTN_NES_DEBUG_PPU           1003
-#define IDC_GRP_WATCH_BPOINT            1004
-#define IDC_EDT_WATCH                   1005
-#define IDC_BTN_ADD_WATCH               1006
-#define IDC_BTN_DEL_WATCH               1007
-#define IDC_BTN_DEL_ALL_WATCH           1008
-#define IDC_BTN_DEL_ALL_BPOINT          1009
-#define IDC_GRP_CPU_CONTROL             1010
-#define IDC_RDO_CPU_RUN_DEBUG           1011
-#define IDC_RDO_CPU_RUN_NSTEPS          1012
-#define IDC_EDT_NSTEPS                  1013
-#define IDC_BTN_CPU_STEP_IN             1014
-#define IDC_BTN_CPU_STEP_OUT            1015
-#define IDC_BTN_CPU_STEP_OVER           1016
-#define IDC_BTN_CPU_TRACKING            1017
-#define IDC_CHECK_AUTO_DASM             1018
-#define IDC_EDT_LIST_CTRL               1019
-#define IDC_LST_OPCODE                  1020
+#define IDC_BTN_NES_DEBUG_APU           1004
+#define IDC_BTN_NES_DEBUG_MEM           1005
+#define IDC_BTN_NES_DEBUG_JOYSTICK      1006
+#define IDC_BTN_NES_DEBUG_SYSINFO       1007
+#define IDC_BTN_NES_DEBUG_DUMPER        1008
+#define IDC_GRP_WATCH_BPOINT            1009
+#define IDC_EDT_WATCH                   1010
+#define IDC_BTN_ADD_WATCH               1011
+#define IDC_BTN_DEL_WATCH               1012
+#define IDC_BTN_DEL_ALL_WATCH           1013
+#define IDC_BTN_DEL_ALL_BPOINT          1014
+#define IDC_GRP_CPU_CONTROL             1015
+#define IDC_RDO_CPU_RUN_DEBUG           1016
+#define IDC_RDO_CPU_RUN_NSTEPS          1017
+#define IDC_EDT_NSTEPS                  1018
+#define IDC_BTN_CPU_STEP_IN             1019
+#define IDC_BTN_CPU_STEP_OUT            1020
+#define IDC_BTN_CPU_STEP_OVER           1021
+#define IDC_BTN_CPU_TRACKING            1022
+#define IDC_CHECK_AUTO_DASM             1023
+#define IDC_EDT_LIST_CTRL               1024
+#define IDC_LST_OPCODE                  1025
 #define ID_DELBREAKPOINT                32771
 #define ID_ADDBREAKPOINT                32772
 #define ID_DASMLIST_SELECTALL           32773
@@ -39,7 +44,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32776
-#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_CONTROL_VALUE         1026
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
